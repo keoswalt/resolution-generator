03aab6d1684a05f8385bea262a7e8d901a8516d9
Kim Oswalt <130675196+keoswalt@users.noreply.github.com>
2024-03-01 09:25:04 -0600

    Fixed generated sentence

--- main.js
+++ main.js
@@ -15,30 +15,23 @@ const selectValue = function(array) {
     return array[i];
 }
 
-// Selecting empty spaces in DOM
-const space1 = document.querySelector('.part1');
-const space2 = document.querySelector('.part2');
-const space3 = document.querySelector('.part3');
-const space4 = document.querySelector('.part4');
+// Selecting element in DOM
+const generatedSentenceWrapper = document.querySelector('.generatedSentenceWrapper');
 
+// Sentence generator
 
 const generateSentence = function () {
   let first = selectValue(part1);
   let second = selectValue(part2);
   let third = selectValue(part3);
   let fourth = selectValue(part4);
-  space1.textContent = first;
-  space2.textContent = second;
-  space3.textContent = third;
-  space4.textContent = fourth;
+
+  generatedSentenceWrapper.innerHTML = "<p>This year, I want to " + first + " " + second + " " + third + " " + fourth + "."</p>;
 };
+
+// Buttons
 
 const generateButton = document.querySelector("#generateNew");
 generateButton.addEventListener("click", generateSentence);
-
-// console.log(generateSentence());
-
-// return "This year, I want to " + first + " " + second + " " + third + " " + fourth + ".";
-
 
 
